0800033c6f23d899d81e2b7fc98bf57a9cab75bf
gabibianca <148897392+gabibianca@users.noreply.github.com>
2023-10-31 13:51:57 -0300

    Adicionados os arquivos html.js ecss

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(400, 400);
+  
+   background("pink");
+  
+  }
+  
+  function draw(){
+    stroke('rgb(0,0,0)')
+    fill('rgb(142,215,240)')
+    
+    if(mouseIsPressed)
+      rect(mouseX,mouseY,20,20);}
+    
+  
+  
+  
+  
